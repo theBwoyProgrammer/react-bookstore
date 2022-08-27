776c20863e4b6a610059b893a98108a69d18426d
Codeaddictjaver <102610715+Javernick@users.noreply.github.com>
2022-08-27 03:38:38 +0300

    styling

--- src/components/Books.js
+++ src/components/Books.js
@@ -3,6 +3,8 @@ import React from 'react';
 import { useDispatch } from 'react-redux';
 import { PropTypes } from 'prop-types';
 import { deleteBookFetch } from '../redux/book/book';
+import { FaCircleNotch } from 'react-icons/fa';
+
 
 const BookCollection = ({ item_id, title, author }) => {
   const dispatch = useDispatch();
@@ -12,18 +14,35 @@ const BookCollection = ({ item_id, title, author }) => {
       dispatch(deleteBookFetch(targetId))
   };
   return (
-    <div key={item_id}>
-      <div>{title}</div>
-      <div>{author}</div>
-      <button
+    <div key={item_id} className='container'>
+      <div className='title'>{title}</div>
+      <div className='author'>{author}</div>
+      <div className='btn-wrapper'>
+      <span>Comment</span>
+      <span
             className="delete-btn"
             id={item_id}
             type="button"
             onClick={(e) => handleDelete(e)}
           >
             Remove
-          </button>
-
+          </span>
+          <span>Edit</span>
+          </div>
+          <div className='progress'>
+          <p className='prog-icons'><FaCircleNotch size={60} style={{ color: 'blue' }} />
+          <span className='percent'>
+          <span>65%</span>
+          <span>Completed</span>
+          </span>
+          </p>
+          <div className='divider'></div>
+          <div className='chap'>
+            <span className='chapname'>Current Chapter</span>
+            <span className='chapnum'>Chapter 6</span>
+            <span className='btnlike'>Update Progress</span>
+          </div>
+          </div>
     </div>
   );
 };
